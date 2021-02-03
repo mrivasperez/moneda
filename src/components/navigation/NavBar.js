3f88b818190cacf0797ed4cb1040e73b9d3d918d
Miguel Rivas Perez <mrivasperez@icloud.com>
2021-02-02 16:44:33 -0800

    update style  NavBar

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -3,12 +3,17 @@ import styled from "styled-components";
 import NavButton from "./NavButton";
 
 const Logo = styled.div`
-  font-size: 1.6em;
+  font-size: 1.8em;
+  color: #ab6ea6;
+  font-weight: bold;
+  cursor: default;
 `;
 
 const Bar = styled.div`
   display: grid;
+  align-items: center;
   grid-template-columns: 180px auto 100px 100px;
+  margin-bottom: 40px;
 `;
 
 function NavBar() {
--- src/components/navigation/NavButton.js
+++ src/components/navigation/NavButton.js
@@ -1,13 +1,20 @@
 import React from "react";
 import styled, { css } from "styled-components";
 
-const NavButtonElem = styled.div`
+const NavButtonElem = styled.p`
   cursor: pointer;
+  color: #0e1c1e;
+  padding: 10px;
+  text-alight: center;
+  margin: 0 10px 0 10px;
+
   ${({ active }) =>
     active &&
     css`
-      color: blue;
-    `}
+      text-decoration: underline;
+      text-decoration-color: #ab6ea6;
+      text-decoration-thickness: 3px;
+    `};
 `;
 
 function NavButton({ name, active }) {
